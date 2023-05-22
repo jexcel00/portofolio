408737cee0a6c99d2d453ee82858686df2cf31a9
lizzaraga <fomekongchristmael@gmail.com>
2023-05-22 05:30:10 +0100

    Change navbar background to surface theme

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="flex items-center justify-between px-8 py-3 border-b border-gray-200">
     <h1 class="text-lg font-bold">Portfolio</h1>
-    <button @click="onToggleButtonClicked" class="flex items-center justify-center p-2 rounded-full bg-slate-100 lg:hidden"><span class="material-icons">menu</span></button>
-    <div ref="linksParentRef" class="fixed top-0 bottom-0 left-0 z-50 hidden p-4 bg-white shadow-lg lg:block lg:relative lg:w-auto w-[350px] lg:p-0 lg:shadow-none">
+    <button @click="onToggleButtonClicked" class="flex items-center justify-center p-2 text-black rounded-full bg-slate-100 lg:hidden"><span class="material-icons">menu</span></button>
+    <div ref="linksParentRef" class="fixed top-0 bottom-0 left-0 z-50 hidden p-4 bg-surface shadow-lg lg:block lg:relative lg:w-auto w-[350px] lg:p-0 lg:shadow-none">
       <ul  class="flex flex-col items-center gap-8 lg:flex-row">
       <li v-for="link in links" :key="link.href">
         <a @click="onLinkClicked(link.href)" :class="{'text-primary font-semibold': link.href == hash}" :href="link.href">{{ link.label }}</a>
